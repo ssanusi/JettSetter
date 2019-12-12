c6fe84d5c27ff21edd321bca4e3098880d8b05eb
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2019-12-12 21:35:54 +0100

    local state

---
+++ src/components/NewItem.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import uniqueId from 'lodash/uniqueId';
+
+import './NewItem.css';
+
+class NewItem extends Component {
+  state = { value: '' };
+
+  handleChange = event => {
+    const { target:{ value } } = event;
+    this.setState({ value })
+  };
+
+  handleSubmit = event => {
+    const { onSubmit } = this.props;
+    const { value } = this.state;
+
+    event.preventDefault();
+
+    onSubmit({ value, id:uniqueId, packed: false});
+
+      this.setState({ value : ''})
+  };
+
+  render() {
+    const { value } = this.state;
+
+    return (
+      <form className="NewItem" onSubmit={this.handleSubmit}>
+        <input
+          className="NewItem-input"
+          type="text"
+          value={value}
+          onChange={this.handleChange}
+        />
+        <input className="NewItem-submit button" type="submit" />
+      </form>
+    );
+  }
+}
+
+export default NewItem;
